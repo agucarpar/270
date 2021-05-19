de893e94a57a22c540e89c4449d96be9a3afcdbc
Agucarpar <agucp@outlook.es>
2021-05-19 19:45:09 +0200

    clean code

---
+++ src/components/Claim.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="claim-wrapper">
+    <div class="claim-wrapper__claim">
+      <p> A <b>NEW WAY</b> TO LEARN MUSIC.</p>
+    </div>
+    <div class="claim-wrapper__blinker">
+      <Blinker/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blinker from '@/components/Blinker.vue'
+export default {
+  name:"Claim",
+  components: {
+    Blinker
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+  .claim-wrapper {
+    position: relative;
+    top: 5rem;
+    display: flex;
+    flex-direction:column ;
+    &__claim {
+      color: $colorLandingText;
+      border: 10px solid $colorLandingText;
+      padding: 1rem;
+      width: 26rem;
+      text-align: center;
+    }
+    &__blinker {
+      animation: blinkerText 1.2s infinite;
+    }
+  }
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo-wrapper">
+    <div>
+      <p>SOUNDNEXT_</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Logo",
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+  .logo-wrapper {
+    text-align: center;
+    font-size: 5rem;
+    color: $colorLandingText;
+  }
+</style>
