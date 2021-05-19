a270f2948c31cfbdb0c369ec63fc76dfa83fe5bb
Agucarpar <agucp@outlook.es>
2021-05-19 19:44:43 +0200

    make text blink

---
+++ src/components/Blinker.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="logo-wrapper">
+    <div>
+      <p>Scroll down to discover more</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Blinker",
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+  .logo-wrapper {
+    text-align: center;
+  }
+</style>
